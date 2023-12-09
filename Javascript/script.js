6ad50539dc8861296d16c10fa8532a23602dfd67
Michael Ponce <123281251+mikerusensei@users.noreply.github.com>
2023-12-09 16:39:10 +0800

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,134 @@
+function playAudio1(){
+    var audio1 = document.getElementById('audio1')
+    audio1.play();
+}
+function pauseAudio1(){
+    var audio1 = document.getElementById('audio1')
+    audio1.pause();
+}
+
+function playAudio2(){
+    var audio2 = document.getElementById('audio2')
+    audio2.play();
+}
+function pauseAudio2(){
+    var audio2 = document.getElementById('audio2')
+    audio2.pause();
+}
+
+function playAudio3(){
+    var audio3 = document.getElementById('audio3')
+    audio3.play()
+}
+function pauseAudio3(){
+    var audio3 = document.getElementById('audio3')
+    audio3.pause();
+}
+
+function playAudio4(){
+    var audio4 = document.getElementById('audio4')
+    audio4.play()
+}
+function pauseAudio4(){
+    var audio4 = document.getElementById('audio4')
+    audio4.pause();
+}
+
+function playAudio5(){
+    var audio5 = document.getElementById('audio5')
+    audio5.play()
+}
+function pauseAudio5(){
+    var audio5 = document.getElementById('audio5')
+    audio5.pause();
+}
+
+function playAudio6(){
+    var audio6 = document.getElementById('audio6')
+    audio6.play()
+}
+function pauseAudio6(){
+    var audio6 = document.getElementById('audio6')
+    audio6.pause();
+}
+
+function playAudio7(){
+    var audio7 = document.getElementById('audio7')
+    audio7.play()
+}
+function pauseAudio7(){
+    var audio7 = document.getElementById('audio7')
+    audio7.pause();
+}
+
+function playAudio8(){
+    var audio8 = document.getElementById('audio8')
+    audio8.play()
+}
+function pauseAudio8(){
+    var audio8 = document.getElementById('audio8')
+    audio8.pause();
+}
+
+function playAudio9(){
+    var audio9 = document.getElementById('audio9')
+    audio5.play()
+}
+function pauseAudio9(){
+    var audio9 = document.getElementById('audio9')
+    audio9.pause();
+}
+
+function playAudio10(){
+    var audio10 = document.getElementById('audio10')
+    audio10.play()
+}
+function pauseAudio10(){
+    var audio10 = document.getElementById('audio10')
+    audio10.pause();
+}
+
+function playAudio11(){
+    var audio11 = document.getElementById('audio11')
+    audio11.play()
+}
+function pauseAudio11(){
+    var audio11 = document.getElementById('audio11')
+    audio11.pause();
+}
+
+function playAudio12(){
+    var audio12 = document.getElementById('audio12')
+    audio12.play()
+}
+function pauseAudio12(){ 
+    var audio12 = document.getElementById('audio12')
+    audio12.pause();
+}
+
+function playAudio13(){
+    var audio13 = document.getElementById('audio13')
+    audio13.play()
+}
+function pauseAudio13(){
+    var audio13 = document.getElementById('audio13')
+    audio13.pause();
+}
+
+function playAudio14(){
+    var audio14 = document.getElementById('audio14')
+    audio14.play()
+}
+function pauseAudio14(){
+    var audio14 = document.getElementById('audio14')
+    audio14.pause();
+}
+
+function playAudio15(){
+    var audio15 = document.getElementById('audio15')
+    audio15.play()
+}
+function pauseAudio15(){
+    var audio15 = document.getElementById('audio15')
+    audio15.pause();
+}
